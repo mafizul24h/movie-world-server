13a26bb1928dc6ef9e76fbd4c7e67892bb5fab12
mafizul24h <mafizul24h@gmail.com>
2024-01-14 23:42:43 +0600

    README.md file Update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const movieCollections = client.db('movieDB').collection('movies');
         const upcommingMovieCollections = client.db('movieDB').collection('upcomingMovie');
